6143bee434e61573dea2d9f56a95560afcb20cfc
millionhz <m.waqas.hamza@gmail.com>
2022-06-05 15:53:28 +0500

    setup eslint with prettier; change lf to crlf; reformat everything

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,12 +4,24 @@ module.exports = {
     es2021: true,
     node: true,
   },
-  extends: ['eslint:recommended', 'airbnb-base'],
+  extends: ['eslint:recommended', 'airbnb-base', 'prettier'],
+  plugins: ['prettier'],
   parserOptions: {
     ecmaVersion: 'latest',
   },
   rules: {
+    'linebreak-style': ['error', 'windows'],
     quotes: ['error', 'single'],
     semi: ['error', 'always'],
+    'prettier/prettier': [
+      'error',
+      {
+        endOfLine: 'auto',
+        singleQuote: true,
+      },
+    ],
   },
 };
+
+// https://www.robinwieruch.de/prettier-eslint/
+// https://stackoverflow.com/questions/53516594/why-do-i-keep-getting-delete-cr-prettier-prettier
