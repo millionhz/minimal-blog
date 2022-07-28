367568abb961dcfe1d4aa5f195824e9fb08bc038
millionhz <m.waqas.hamza@gmail.com>
2022-07-28 17:34:06 +0500

    added exception handling to /post

--- routes/post.js
+++ routes/post.js
@@ -5,13 +5,21 @@ const Post = require('../models/post');
 const router = express.Router();
 
 router.get('/:id', (req, res, next) => {
-  Post.getPostById(req.params.id).then((post) => {
-    if (post) {
-      res.render('post', { post });
-    } else {
-      next();
-    }
-  });
+  Post.getPostById(req.params.id)
+    .then((post) => {
+      if (post) {
+        res.render('post', { post });
+      } else {
+        next();
+      }
+    })
+    .catch((err) => {
+      if (err.name === 'CastError') {
+        next();
+      } else {
+        next(err);
+      }
+    });
 });
 
 module.exports = router;
